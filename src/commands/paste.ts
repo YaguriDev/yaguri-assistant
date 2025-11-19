9ab433aef4a57bf843e92056169f048c620ed7a3
reCAPTCHA <53586412+KoldaN1@users.noreply.github.com>
2025-11-19 04:02:49 +0200

    fix paste text
    uses exec powershell script

--- src/commands/paste.ts
+++ src/commands/paste.ts
@@ -1,15 +1,15 @@
-import { keyboard, Key, clipboard } from "@nut-tree-fork/nut-js";
+import { clipboard } from "@nut-tree-fork/nut-js";
+import { exec } from "child_process";
 
 export const pasteText = async (text: string) => {
   await clipboard.setContent(text);
-
   await new Promise((res) => setTimeout(res, 100));
+  pasteTextFunc();
+};
 
-  await keyboard.pressKey(Key.LeftControl);
-  await keyboard.pressKey(Key.V);
-
-  await keyboard.releaseKey(Key.V);
-  await keyboard.releaseKey(Key.LeftControl);
-
-  await new Promise((res) => setTimeout(res, 20));
+export const pasteTextFunc = async () => {
+  return new Promise<void>((resolve, reject) => {
+    const cmd = `powershell -command "$wshell = New-Object -ComObject wscript.shell; $wshell.SendKeys('^v')"`;
+    exec(cmd, (err) => (err ? reject(err) : resolve()));
+  });
 };
